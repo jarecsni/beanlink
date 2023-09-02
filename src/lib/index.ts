8636fbed6e3050f6dfa2916a2dde3737e154366e
Janos Jarecsni <janos.jarecsni@gmail.com>
2023-09-02 08:25:56 +0100

    Fixing reexports

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+// Reexport your entry components here
+import { BeanLink } from './BeanLink.js';
+import type { Feature } from './Feature.js';
+import { FeatureManager } from './FeatureManager.js';
+
+export { BeanLink, Feature, FeatureManager };
