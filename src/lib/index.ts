0e0011fb66d4fa45d7296331b09142a7c4ec22a0
Janos Jarecsni <janos.jarecsni@gmail.com>
2023-09-02 08:59:23 +0100

    Added all existing exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,20 @@
 // Reexport your entry components here
-import { BeanLink } from './BeanLink.js';
+import { 
+    BeanLink, 
+    type BeanLinkEvent,
+    type BeanLinkEventCreator, 
+    type BeanLinkEventHandler,
+    createEvent
+} from './BeanLink.js';
 import type { Feature } from './Feature.js';
 import { FeatureManager } from './FeatureManager.js';
 
-export { BeanLink, Feature, FeatureManager };
+export { 
+    BeanLink,
+    BeanLinkEvent, 
+    BeanLinkEventCreator,
+    BeanLinkEventHandler,
+    createEvent,
+    Feature, 
+    FeatureManager 
+};
