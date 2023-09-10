44da3a12868aa308246e1a9f4f71d6599875e1ce
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-09-10 20:13:35 +0100

    Documentation

--- src/lib/FeatureManager.ts
+++ src/lib/FeatureManager.ts
@@ -2,7 +2,7 @@ import type { Feature } from './Feature';
 
 /**
  * Register `Feature`s with BeanLink.
- * This class is used for registering your `Feature`s in your app - `FeatureManager` in turn will call the {@link setup} method in your `Feature`.
+ * This class is used for registering your `Feature`s in your app - `FeatureManager` in turn will call the {@link Feature#setup} method in your `Feature`.
  */
 export class FeatureManager {
     private static _instance:FeatureManager = new FeatureManager();
@@ -18,7 +18,7 @@ export class FeatureManager {
 
     /**
      * Registers the specified `Feature` with the `FeatureManager`.
-     * @param feature - the {@link Feaure} to register
+     * @param feature - the {@link Feature} to register
      */
     public registerFeature(feature:Feature) {
         this.features.set(feature.name, feature);
